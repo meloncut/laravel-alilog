d738fd89440cfe98b42f7fb4e05fb1092615b0bd
DizzyLee <xuzhu@changjinglu.net>
2019-10-25 10:29:42 +0800

    fix

--- src/AliLog/AliCloudLogHandler.php
+++ src/AliLog/AliCloudLogHandler.php
@@ -16,11 +16,7 @@ class AliCloudLogHandler extends AbstractProcessingHandler
 {
     protected $invoker;
 
-    const ALI_LOG_HANDLE_SYNC = 'sync';
-
-    const ALI_LOG_HANDLE_ASYNC = 'async';
-
-    public function __construct(ALiCloudLogInvoker $invoker,\Aliyun_Log_Models_PutLogsRequest $request,\int $level = Logger::DEBUG, bool $bubble = true)
+    public function __construct(ALiCloudLogInvoker $invoker,\int $level = Logger::DEBUG, bool $bubble = true)
     {
         $this->invoker = $invoker;
         parent::__construct($level, $bubble);
@@ -28,10 +24,6 @@ class AliCloudLogHandler extends AbstractProcessingHandler
 
     protected function write(array $record)
     {
-        if (env('ALI_LOG_DRIVE' == self::ALI_LOG_HANDLE_ASYNC)) {
-            $this->syncLog($record);
-        }
-
         $this->syncLog($record);
     }
 
@@ -45,29 +37,4 @@ class AliCloudLogHandler extends AbstractProcessingHandler
     {
         $this->invoker->sendLog($record);
     }
-
-    /**
-     * send async log to queue
-     *
-     * @param array $record
-     * @author <meloncut@outlook.com>
-     */
-    protected function asyncLog(array $record)
-    {
-
-    }
-
-    /**
-     * create a ali log item
-     *
-     * @param array $record
-     * @return \Aliyun_Log_Models_LogItem
-     * @author <meloncut@outlook.com>
-     */
-    protected function packageItem(array $record)
-    {
-       return new \Aliyun_Log_Models_LogItem(null, $record);
-    }
-
-
 }
